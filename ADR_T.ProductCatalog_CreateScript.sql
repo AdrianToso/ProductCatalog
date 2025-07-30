f09058bf315fdbfe470cc17b4f3ad69c3165191d
Adrian Toso <adrtoso@gmail.com>
2025-07-30 19:47:28 -0300

    - Se agregan las entidades principales: Product, Category y EntityBase, con sus propiedades y validaciones. - Se implementa la relación muchos-a-muchos entre productos y categorías. - Se configura el contexto de base de datos (AppDbContext) y las clases de configuración para EF Core. - Se añade la migración inicial y el snapshot del modelo para la estructura de la base de datos. - Se incluye el controlador ProductsController con endpoint para crear productos. - Se agrega el comando CreateProductCommand. - Se incorpora el Dockerfile para la WebApi con soporte para .NET 8.

---
+++ ADR_T.ProductCatalog_CreateScript.sql
@@ -0,0 +1,50 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Categories] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(100) NOT NULL,
+    [Description] nvarchar(max) NULL,
+    [FechacCreacion] datetime2 NOT NULL,
+    [FechacActualizacion] datetime2 NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Products] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(200) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    [ImageUrl] nvarchar(max) NULL,
+    [FechacCreacion] datetime2 NOT NULL,
+    [FechacActualizacion] datetime2 NULL,
+    [IsDeleted] bit NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [ProductCategories] (
+    [CategoriesId] uniqueidentifier NOT NULL,
+    [ProductsId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_ProductCategories] PRIMARY KEY ([CategoriesId], [ProductsId]),
+    CONSTRAINT [FK_ProductCategories_Categories_CategoriesId] FOREIGN KEY ([CategoriesId]) REFERENCES [Categories] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_ProductCategories_Products_ProductsId] FOREIGN KEY ([ProductsId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX [IX_Categories_Name] ON [Categories] ([Name]);
+
+CREATE INDEX [IX_ProductCategories_ProductsId] ON [ProductCategories] ([ProductsId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250729163619_InitialCreate', N'9.0.7');
+
+COMMIT;
+GO
+
